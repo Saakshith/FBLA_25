44eedfbfeb5b5eb29d701aa8032c804fe2c5ea2c
Saakshith <saaks0524@outlook.com>
2025-01-27 23:52:19 -0800

    made some changes

--- Documents/FBLA_25/website/src/App.js
+++ Documents/FBLA_25/website/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import StudentLandingPage from "./student_landing_page/StudentLandingPage"
 import StudentSignIn from './student_signin/StudentSignIn';
+import StudentSignUp from "./student_signup/StudentSignUp"
 import FindJobs from "./find_jobs/FindJobs"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<StudentLandingPage />} />
           <Route path="/signin" element={<StudentSignIn />} />
+          <Route path="/signup" element={<StudentSignUp />} />
           <Route path="/findjobs" element={<FindJobs />} />
         </Routes>
       </BrowserRouter>
